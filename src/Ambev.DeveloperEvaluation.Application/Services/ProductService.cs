e1f3a2b7253ba800c8a5904f6e80d251f479572e
matheuscosta2 <matheuscostajgppo@gmail.com>
2025-04-13 11:36:58 -0300

    feat: services created

---
+++ src/Ambev.DeveloperEvaluation.Application/Services/ProductService.cs
@@ -0,0 +1,216 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Base;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+using Ambev.DeveloperEvaluation.Domain.Exceptions;
+using Ambev.DeveloperEvaluation.Domain.Interfaces.Repositories;
+using Ambev.DeveloperEvaluation.Domain.Interfaces.Services;
+using FluentValidation;
+using Microsoft.Extensions.Logging;
+
+namespace Ambev.DeveloperEvaluation.Application.Services;
+
+public class ProductService : IProductService
+{
+    private readonly IProductRepository _repository;
+    private readonly IBranchProductRepository _branchProductRepository;
+    private readonly IValidator<Product> _validator;
+    private readonly ILogger<ProductService> _logger;
+
+    public ProductService(IProductRepository repository,
+                          IBranchProductRepository branchProductRepository,
+                          IValidator<Product> validator,
+                          ILogger<ProductService> logger)
+    {
+        _repository = repository;
+        _branchProductRepository = branchProductRepository;
+        _validator = validator;
+        _logger = logger;
+    }
+
+    public async Task<Product> CreateAsync(Product request)
+    {
+        try
+        {
+            request.Rating ??= new ProductRating();
+
+            await ValidateProductAsync(request);
+
+            return await _repository.AddAsync(request);
+        }
+        catch (Exception ex) when (ex is ValidationException || ex is BaseException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new ServiceException("An error occurred while creating a product.", ex);
+        }
+    }
+
+    public async Task<IEnumerable<string>> GetAllCategoriesAsync()
+    {
+        var categories = Enum.GetValues(typeof(ProductCategory))
+                              .Cast<ProductCategory>()
+                              .Select(c => c.ToString());
+
+        return await Task.FromResult(categories);
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        try
+        {
+            var product = await FindProductOrThrowAsync(id);
+
+            await _repository.DeleteAsync(product);
+        }
+        catch (BaseException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new ServiceException("An error occurred while deleting the product.", ex);
+        }
+    }
+
+    public async Task<PagedResult<Product>> GetAllAsync(int? id = default,
+                                                        bool? isActive = default,
+                                                        string? title = default,
+                                                        string? category = default,
+                                                        decimal? minPrice = default,
+                                                        decimal? maxPrice = default,
+                                                        DateTimeOffset? startDate = default,
+                                                        DateTimeOffset? endDate = default,
+                                                        int page = 1,
+                                                        int maxResults = 10,
+                                                        string? orderByClause = default)
+    {
+        try
+        {
+            if (page <= 0 || maxResults <= 0)
+                throw new InvalidPaginationParametersException("Page number and max results must be greater than zero.");
+
+            var criteria = BuildCriteria(id, isActive, title, category, minPrice, maxPrice, startDate, endDate);
+
+            var result = await _repository.GetAsync(page, maxResults, criteria, orderByClause);
+
+            return result;
+        }
+        catch (BaseException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new ServiceException("An error occurred while retrieving products.", ex);
+        }
+    }
+
+    public async Task<Product?> GetByIdAsync(int id)
+    {
+        try
+        {
+            var product = await _repository.GetByIdAsync(id);
+
+            return product;
+        }
+        catch (Exception ex)
+        {
+            throw new ServiceException("An error occurred while retrieving the product.", ex);
+        }
+    }
+
+    public async Task<Product> UpdateAsync(int id, Product request)
+    {
+        try
+        {
+            var existingProduct = await FindProductOrThrowAsync(id);
+
+            var oldTitle = existingProduct.Title!;
+            var oldCategory = existingProduct.Category;
+
+            var product = await UpdateProductAsync(existingProduct, request);
+
+            await ValidateProductAsync(product);
+
+            await _repository.UpdateAsync(product);
+
+            if (!oldTitle.Equals(product.Title) || oldCategory != product.Category)
+                await _branchProductRepository.UpdateByProductIdAsync(product.Id, product.Title!, product.Category);
+
+            return product;
+        }
+        catch (Exception ex) when (ex is ValidationException || ex is BaseException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new ServiceException("An error occurred while updating the product.", ex);
+        }
+    }
+
+    private async Task<Product> UpdateProductAsync(Product existingProduct, Product request)
+    {
+        existingProduct.Title = request.Title;
+        existingProduct.Description = request.Description;
+        existingProduct.Image = request.Image;
+        existingProduct.Category = request.Category;
+        existingProduct.Price = request.Price;
+        existingProduct.IsActive = request.IsActive;
+
+        if (request.Rating is not null)
+            existingProduct.Rating = request.Rating;
+
+        return await Task.FromResult(existingProduct);
+    }
+
+    private Expression<Func<Product, bool>> BuildCriteria(int? id,
+                                                          bool? isActive,
+                                                          string? title,
+                                                          string? category,
+                                                          decimal? minPrice,
+                                                          decimal? maxPrice,
+                                                          DateTimeOffset? startDate,
+                                                          DateTimeOffset? endDate)
+    {
+        ProductCategory? categoryFilter = default;
+
+        if (!string.IsNullOrWhiteSpace(category))
+            if (Enum.TryParse<ProductCategory>(category, true, out var categoryEnum))
+                categoryFilter = categoryEnum;
+
+        return b =>
+            (!id.HasValue || b.Id == id.Value) &&
+            (!isActive.HasValue || b.IsActive == isActive.Value) &&
+            (string.IsNullOrEmpty(title) ||
+            (title.StartsWith("*") && title.EndsWith("*") ? b.Title!.Contains(title.Trim('*')) :
+            title.StartsWith("*") ? b.Title!.EndsWith(title.TrimStart('*')) :
+            title.EndsWith("*") ? b.Title!.StartsWith(title.TrimEnd('*')) :
+            b.Title == title)) &&
+            (!categoryFilter.HasValue || b.Category == categoryFilter.Value) &&
+            (!minPrice.HasValue || b.Price >= minPrice.Value) &&
+            (!maxPrice.HasValue || b.Price <= maxPrice.Value) &&
+            (!startDate.HasValue || b.CreatedAt >= startDate.Value) &&
+            (!endDate.HasValue || b.CreatedAt <= endDate.Value);
+    }
+
+    private async Task<Product> FindProductOrThrowAsync(int id)
+    {
+        var product = await _repository.GetByIdAsync(id);
+
+        if (product is null)
+            throw new NotFoundException($"Product with ID {id} not found.");
+
+        return product;
+    }
+
+    private async Task ValidateProductAsync(Product request)
+    {
+        var validationResult = await _validator.ValidateAsync(request);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Services/SaleService.cs
@@ -0,0 +1,434 @@
+﻿using Ambev.DeveloperEvaluation.Application.Events.Sales;
+using Ambev.DeveloperEvaluation.Domain.Base;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+using Ambev.DeveloperEvaluation.Domain.Exceptions;
+using Ambev.DeveloperEvaluation.Domain.Interfaces.Intergrations;
+using Ambev.DeveloperEvaluation.Domain.Interfaces.Repositories;
+using Ambev.DeveloperEvaluation.Domain.Interfaces.Services;
+using FluentValidation;
+using Microsoft.Extensions.Logging;
+using System.Linq.Expressions;
+
+namespace Ambev.DeveloperEvaluation.Application.Services;
+
+public class SaleService : ISaleService
+{
+    private const int MAX_ITEMS_PER_SALE = 20;
+
+    private readonly ISaleRepository _repository;
+    private readonly ISaleItemRepository _saleItemRepository;
+    private readonly IBranchProductRepository _branchProductRepository;
+    private readonly IValidator<Sale> _validator;
+    private readonly IRabbitMQIntegration _rabbitMQIntegration;
+    private readonly ILogger<SaleService> _logger;
+
+    public SaleService(ISaleRepository repository,
+                       ISaleItemRepository saleItemRepository,
+                       IBranchProductRepository branchProductRepository,
+                       IValidator<Sale> validator,
+                       IRabbitMQIntegration rabbitMQIntegration,
+                       ILogger<SaleService> logger)
+    {
+        _repository = repository;
+        _saleItemRepository = saleItemRepository;
+        _branchProductRepository = branchProductRepository;
+        _validator = validator;
+        _rabbitMQIntegration = rabbitMQIntegration;
+        _logger = logger;
+    }
+
+    public async Task<Sale> CreateAsync(Sale request)
+    {
+        try
+        {
+            var sale = new Sale
+            {
+                BranchId = request.BranchId,
+                UserId = request.UserId,
+                Date = DateTime.UtcNow,
+                Status = SaleStatus.Created,
+                Items = new List<SaleItem>()
+            };
+
+            await ProcessItemsAsync(sale, request.Items!);
+
+            await ValidateSaleAsync(sale);
+
+            var savedSale = await _repository.AddAsync(sale);
+
+            await UpdateStockQuantitiesAsync(savedSale.Items!, savedSale.BranchId);
+
+            await PublishSaleMessageAsync(new SaleCreatedEvent(savedSale));
+
+            return savedSale;
+        }
+        catch (Exception ex) when (ex is ValidationException || ex is BaseException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new ServiceException("An error occurred while processing sale. Please try again later.", ex);
+        }
+    }
+
+    public async Task<Sale> CancelItemAsync(int saleId, int sequence)
+    {
+        try
+        {
+            var sale = await GetSaleWithItemsOrThrowAsync(saleId);
+
+            if (sale.Status == SaleStatus.Canceled)
+                throw new SaleAlreadyCanceledException($"Cannot cancel an item from a sale that is already canceled.");
+
+            var saleItem = sale?.Items?.FirstOrDefault(i => i.Sequence == sequence);
+
+            if (saleItem is null)
+                throw new NotFoundException($"Sale item sequence {sequence} not found.");
+
+            ValidateItemForCancellation(saleItem);
+
+            CancelItem(saleItem);
+
+            sale!.TotalAmount = CalculateTotalAmount(sale.Items!);
+
+            await _repository.UpdateAsync(sale);
+
+            await PublishSaleMessageAsync(new SaleItemCancelledEvent(saleItem));
+
+            return sale;
+        }
+        catch (BaseException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new ServiceException("An error occurred while canceling the sale item.", ex);
+        }
+    }
+
+    public async Task<SaleItem> GetItemAsync(int saleId, int sequence)
+    {
+        try
+        {
+            var sale = await GetSaleWithItemsOrThrowAsync(saleId);
+
+            var saleItem = sale?.Items?.FirstOrDefault(i => i.Sequence == sequence);
+
+            if (saleItem is null)
+                throw new NotFoundException($"Sale item sequence {sequence} not found in sale ID {saleId}.");
+
+            return saleItem;
+        }
+        catch (BaseException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new ServiceException("An error occurred while retrieving the item.", ex);
+        }
+    }
+
+    public async Task<PagedResult<Sale>> GetAllAsync(int? id = default,
+                                                     int? branchId = default,
+                                                     int? userId = default,
+                                                     SaleStatus? status = default,
+                                                     DateTimeOffset? startDate = default,
+                                                     DateTimeOffset? endDate = default,
+                                                     int page = 1,
+                                                     int maxResults = 10,
+                                                     string? orderByClause = default)
+    {
+        try
+        {
+            if (page <= 0 || maxResults <= 0)
+                throw new InvalidPaginationParametersException("Page number and max results must be greater than zero.");
+
+            var criteria = BuildCriteria(id, branchId, userId, status, startDate, endDate);
+
+            var result = await _repository.GetAsync(page, maxResults, criteria, orderByClause);
+
+            return result;
+        }
+        catch (BaseException ex)
+        {
+            throw new ServiceException("An error occurred while retrieving sales.", ex);
+        }
+        catch (Exception ex)
+        {
+            throw new ServiceException("An error occurred while retrieving sales.", ex);
+        }
+    }
+
+    public async Task<Sale?> GetByIdAsync(int id)
+    {
+        try
+        {
+            var sale = await _repository.GetWithItemsByIdAsync(id);
+
+            return sale;
+        }
+        catch (Exception ex)
+        {
+            throw new ServiceException("An error occurred while retrieving the sale.", ex);
+        }
+    }
+
+    public async Task<Sale> UpdateAsync(int saleId, Sale request)
+    {
+        try
+        {
+            var existingSale = await GetSaleWithItemsOrThrowAsync(saleId);
+
+            ValidateForUpdate(existingSale);
+
+            var updatedSale = UpdateSaleProperties(existingSale, request);
+
+            await ValidateSaleAsync(updatedSale);
+
+            await _repository.UpdateAsync(updatedSale);
+
+            await PublishSaleMessageAsync(new SaleUpdatedEvent(updatedSale));
+
+            return updatedSale;
+        }
+        catch (BaseException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new ServiceException("An error occurred while updating the sale.", ex);
+        }
+    }
+
+    public async Task<Sale> CancelAsync(int saleId)
+    {
+        try
+        {
+            var sale = await _repository.GetByIdAsync(saleId);
+
+            if (sale is null)
+                throw new NotFoundException($"Sale with ID {saleId} not found.");
+
+            if (sale.Status == SaleStatus.Canceled)
+                throw new SaleAlreadyCanceledException($"This sale is already canceled.");
+
+            sale.Status = SaleStatus.Canceled;
+            sale.CancelledAt = DateTimeOffset.Now;
+
+            await _repository.UpdateAsync(sale);
+
+            await PublishSaleMessageAsync(new SaleCancelledEvent(sale));
+
+            return sale;
+        }
+        catch (BaseException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new ServiceException("An error occurred while canceling the sale. Please try again later.", ex);
+        }
+    }
+
+    public async Task DeleteAsync(int saleId)
+    {
+        try
+        {
+            var existingSale = await GetSaleOrThrowAsync(saleId);
+
+            await _repository.DeleteAsync(existingSale);
+        }
+        catch (BaseException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new ServiceException("An error occurred while deleting the sale. Please try again later.", ex);
+        }
+    }
+
+    #region CreateSale
+
+    private async Task ProcessItemsAsync(Sale sale, List<SaleItem> items)
+    {
+        short sequence = 1;
+
+        foreach (var item in items)
+        {
+            var saleItem = await ProcessItemAsync(sale.BranchId, item, sequence);
+            sale.Items!.Add(saleItem);
+            sale.TotalAmount += saleItem.Price;
+
+            sequence++;
+        }
+    }
+
+    private async Task<SaleItem> ProcessItemAsync(int branchId, SaleItem requestItem, short sequence)
+    {
+        var branchProduct = await GetBranchProductOrThrowAsync(branchId, requestItem.ProductId);
+
+        if (branchProduct.StockQuantity < requestItem.Quantity)
+            throw new ItemOutOfStockException($"Product {branchProduct.ProductTitle} is out of stock.");
+
+        if (requestItem.Quantity > MAX_ITEMS_PER_SALE)
+            throw new ItemQuantityLimitExceededException("Cannot sell more than 20 identical items.");
+
+        var saleItem = new SaleItem
+        {
+            ProductId = branchProduct.ProductId,
+            ProductTitle = branchProduct.ProductTitle,
+            UnitPrice = branchProduct.Price,
+            Quantity = requestItem.Quantity,
+            Sequence = sequence
+        };
+
+        saleItem.Discount = CalculateItemDiscount(requestItem);
+
+        saleItem.Price = CalculateItemPrice(saleItem);
+
+        return saleItem;
+    }
+
+    private decimal CalculateItemPrice(SaleItem item)
+    {
+        var discountMultiplier = 1 - (item.Discount / 100 ?? 0);
+        return item.UnitPrice * item.Quantity * discountMultiplier;
+    }
+
+    private decimal CalculateItemDiscount(SaleItem item)
+    {
+        if (item.Quantity < 4)
+            return 0;
+
+        if (item.Discount.HasValue && item.Discount.Value > 0)
+            return item.Discount.Value;
+
+        if (item.Quantity >= 10)
+            return 20;
+
+        return 10;
+    }
+
+    private async Task UpdateStockQuantitiesAsync(List<SaleItem> items, int branchId)
+    {
+        foreach (var item in items)
+        {
+            var branchProduct = await GetBranchProductOrThrowAsync(branchId, item.ProductId);
+
+            branchProduct.StockQuantity -= item.Quantity;
+            await _branchProductRepository.UpdateAsync(branchProduct);
+        }
+    }
+
+    #endregion
+
+    private void ValidateForUpdate(Sale sale)
+    {
+        if (sale.Status == SaleStatus.Canceled)
+            throw new SaleAlreadyCanceledException("Cannot update a canceled sale.");
+    }
+
+    private Sale UpdateSaleProperties(Sale existingSale, Sale request)
+    {
+        var updatedSale = existingSale;
+
+        updatedSale.Status = request.Status;
+        updatedSale.Date = request.Date;
+        updatedSale.UserId = request.UserId;
+        updatedSale.BranchId = request.BranchId;
+        updatedSale.TotalAmount = request.TotalAmount;
+
+        return updatedSale;
+    }
+
+    private void ValidateItemForCancellation(SaleItem saleItem)
+    {
+        if (saleItem.IsCancelled)
+            throw new SaleItemAlreadyCanceledException("This item is already cancelled.");
+    }
+
+    private void CancelItem(SaleItem saleItem)
+    {
+        saleItem.IsCancelled = true;
+        saleItem.CancelledAt = DateTimeOffset.Now;
+    }
+
+    private decimal CalculateTotalAmount(List<SaleItem> items)
+    {
+        return items.Where(item => !item.IsCancelled).Sum(item => item.Price);
+    }
+
+    private async Task<Sale> GetSaleOrThrowAsync(int saleId)
+    {
+        var sale = await _repository.GetByIdAsync(saleId);
+        if (sale is null)
+            throw new NotFoundException($"Sale with ID {saleId} not found.");
+
+        return sale;
+    }
+
+    private async Task<Sale> GetSaleWithItemsOrThrowAsync(int id)
+    {
+        var sale = await _repository.GetWithItemsByIdAsync(id);
+
+        if (sale is null)
+            throw new NotFoundException($"Sale with ID {id} not found.");
+
+        return sale;
+    }
+
+    private async Task PublishSaleMessageAsync(BaseEvent @event)
+    {
+        try
+        {
+            await _rabbitMQIntegration.PublishMessageAsync(@event);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"An error occurred while publishing event {@event.GetType().Name}");
+        }
+    }
+
+    private async Task<BranchProduct> GetBranchProductOrThrowAsync(int branchId, int productId)
+    {
+        var branchProduct = (await _branchProductRepository.GetAsync(1, 1,
+            p => p.IsActive && p.BranchId == branchId && p.ProductId == productId))
+            .Items?.FirstOrDefault();
+
+        if (branchProduct is null)
+            throw new NotFoundException($"Product ID {productId} not found or inactive in branch ID {branchId}.");
+
+        return branchProduct;
+    }
+
+    private async Task ValidateSaleAsync(Sale sale)
+    {
+        var validationResult = await _validator.ValidateAsync(sale);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+    }
+
+    private Expression<Func<Sale, bool>> BuildCriteria(int? id,
+                                                       int? branchId,
+                                                       int? userId,
+                                                       SaleStatus? status,
+                                                       DateTimeOffset? startDate,
+                                                       DateTimeOffset? endDate)
+    {
+        return b =>
+            (!id.HasValue || b.Id == id.Value) &&
+            (!branchId.HasValue || b.BranchId == branchId.Value) &&
+            (!userId.HasValue || b.UserId == userId.Value) &&
+            (!status.HasValue || b.Status == status.Value) &&
+            (!startDate.HasValue || b.CreatedAt >= startDate.Value) &&
+            (!endDate.HasValue || b.CreatedAt <= endDate.Value);
+    }
+}
